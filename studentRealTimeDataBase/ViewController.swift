3f9f8948235e06f31d3dc41ddd139b5b5cb0f29d
Christian-bourquin <christian.bourquin23@d155.org>
2023-01-06 08:06:05 -0600

    tableview for tonight

--- studentRealTimeDataBase/ViewController.swift
+++ studentRealTimeDataBase/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldOutlet: UITextField!
+    
+    @IBOutlet weak var textViewOutlet: UITextView!
+    var ref: DatabaseReference!
+    var names = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        ref = Database.database().reference()
+        //reading from database
+        //automatically gets called at start and when child gets added
+        ref.child("students").observe(.childAdded) { snapshot in
+            
+            var name = snapshot.value as! String
+            
+            self.names.append(name)
+            
+            self.textViewOutlet.text += name + "\n"
+            
+        }
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func buttonAction(_ sender: Any) {
+        var name = textFieldOutlet.text!
+        names.append(name)
+        ref.child("students").childByAutoId().setValue(name)
+    }
+    
 }
 
